b3fb2b3793c593112083fb41a2f0769fc8179cb4
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 16:37:34 +0300

    feat: add solution to "read.js"-task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'fs';
+import { access } from 'fs/promises';
+import { __dirName, SOURCE_FOLDER, ERROR_MSG } from './constants.js';
+
+const FILE_TO_READ = 'fileToRead.txt';
+
+const file_path = `${__dirName}/${SOURCE_FOLDER}/${FILE_TO_READ}`;
+
 const read = async () => {
-    // Write your code here 
+    try {
+        await access(file_path);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
+    
+    const rs = createReadStream(file_path, {encoding: 'utf-8'});
+    rs.pipe(process.stdout);
 };
 
 await read();
