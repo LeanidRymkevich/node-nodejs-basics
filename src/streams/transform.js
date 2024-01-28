d20b8a2e14ef279a4d9e76329548db4b5831173b
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 20:15:29 +0300

    feat: add solution to "stream:transform"-task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+    const ts = new Transform({
+        transform(chunk, encoding, callback) {
+            const str = chunk.toString().trim();
+            const reversedStr = str.split('').reverse().join('');
+            
+            this.push(`${reversedStr}\n`);
+
+            callback();
+        }
+    });
+
+    await pipeline(process.stdin, ts, process.stdout);
 };
 
 await transform();
