1789ac60f2bf3d6c54d40b3a009fb10af1966a14
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 17:11:56 +0300

    feat: add solution to "env.js"-task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    let result = Object.keys(process.env).reduce((prev, curr) => {
+        if (curr.includes(PREFIX)) {
+            return prev += `${curr}=${process.env[curr]}; `;
+        } 
+        return prev;   
+    }, '');
+    result = result ? result.slice(0, -2) : result;
+
+    console.log(result);
 };
 
 parseEnv();
