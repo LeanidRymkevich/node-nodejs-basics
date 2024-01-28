00be9934bbbb23a88affeb0b12b3fcaae4c367ae
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 14:46:40 +0300

    feat: add solution to "copy.js"-task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import { access, readdir, mkdir, copyFile } from 'fs/promises';
+import { __dirName } from './utils.js';
+
+const SOURCE_FOLDER = 'files';
+const DESTINATION_FOLDER = 'files_copy';
+const ERROR_MSG = 'FS operation failed';
+
+const source_path = __dirName + '/' + SOURCE_FOLDER;
+const destination_path = __dirName + '/' + DESTINATION_FOLDER;
+
 const copy = async () => {
-    // Write your code here 
+    let fileNames;
+    try {
+        fileNames = await readdir(source_path);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
+
+    let hasToBeThrown = true;
+    try {
+        await access(destination_path);
+    } catch {
+        hasToBeThrown = false;
+        await mkdir(destination_path);
+    }
+    if (hasToBeThrown) throw new Error(ERROR_MSG);
+
+    fileNames.forEach(async (fileName) => await copyFile(`${source_path}/${fileName}`, `${destination_path}/${fileName}`));
 };
 
 await copy();
