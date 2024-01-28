266646c06df616b64175e365861ac1c3a94dcade
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 15:57:27 +0300

    feat: add solution to "list.js"-task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from 'fs/promises';
+import { __dirName, SOURCE_FOLDER } from './constants.js';
+
+const ERROR_MSG = 'FS operation failed';
+
+const source_path = `${__dirName}/${SOURCE_FOLDER}`;
+
 const list = async () => {
-    // Write your code here 
+    let fileNames;
+
+    try {
+        fileNames = await readdir(source_path);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
+
+    fileNames.forEach(fileName => console.log(fileName));
 };
 
 await list();
