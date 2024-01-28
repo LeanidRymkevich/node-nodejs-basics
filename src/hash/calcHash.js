d64cf57f7a018592b41d80f3db1a92e4f4c05111
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 19:10:15 +0300

    feat: add solution to "calcHash.js"-task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const SOURCE_FOLDER = 'files';
+const SOURCE_FILE = 'fileToCalculateHashFor.txt';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(fileName);
+
+const filePath = `${__dirName}/${SOURCE_FOLDER}/${SOURCE_FILE}`;
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const rs = createReadStream(filePath);
+
+    rs.on('data', data => hash.update(data));
+    rs.on('end', () => console.log(`Hash of '${SOURCE_FILE}' is ${hash.digest('hex')}`));
 };
 
 await calculateHash();
