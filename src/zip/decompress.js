a925288022de8205c7f847ff945008a7d065b3bd
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 21:02:54 +0300

    feat: add solutions to zip-tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib'
+import { pipeline } from 'stream/promises';
+import { __dirName, file_path, archive_path } from './constants.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(archive_path);
+    const writeableStream = createWriteStream(file_path);
+    const unzip = createUnzip();
+
+    await pipeline(readableStream, unzip, writeableStream);
 };
 
 await decompress();
