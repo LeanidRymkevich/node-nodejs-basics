31a96badc7202f681f3fc660ddc00351e9c690ef
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 13:44:48 +0300

    feat: add solution to "create.js"-task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { access, writeFile } from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const DESTINATION_PATH = 'files/fresh.txt';
+const TEXT_TO_WRITE = 'I am fresh and young';
+const FILE_EXISTS_ERROR_MSG = 'FS operation failed';
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(fileName);
+const path = __dirName + '/' + DESTINATION_PATH;
+
 const create = async () => {
-    // Write your code here 
+    let exists = true;
+    try {
+        await access(path);
+    } catch {
+        exists = false;
+    }
+
+    if (exists) throw new Error(FILE_EXISTS_ERROR_MSG);
+
+    await writeFile(path, TEXT_TO_WRITE);
 };
 
 await create();
