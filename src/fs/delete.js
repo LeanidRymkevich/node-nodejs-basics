0e20afc6c0dea87d504e19ed3bf2b6528cf13d7d
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 15:10:06 +0300

    feat: add solution to "delete.js"-task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { access, unlink } from 'fs/promises';
+import { __dirName } from './utils.js';
+
+const FILE_TO_DELETE = 'files/fileToRemove.txt';
+const ERROR_MSG = 'FS operation failed';
+
+const file_path = __dirName + '/' + FILE_TO_DELETE;
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await access(file_path);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
+
+    await unlink(file_path);
 };
 
 await remove();
