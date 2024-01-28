8cf18bcbf0ae643fca6ff5036e27fd5b156928b6
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-28 19:39:53 +0300

    feat: add solution to "stream:write"-task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createWriteStream } from 'fs';
+import { __dirName, SOURCE_FOLDER } from './constants.js';
+
+const FILE_TO_WRITE = 'fileToWrite.txt';
+
+const file_path = `${__dirName}/${SOURCE_FOLDER}/${FILE_TO_WRITE}`;
+
 const write = async () => {
-    // Write your code here 
+    const ws = createWriteStream(file_path, {encoding: 'utf-8', flags: 'a'});
+    process.stdin.on('data', data => ws.write(data));
 };
 
 await write();
