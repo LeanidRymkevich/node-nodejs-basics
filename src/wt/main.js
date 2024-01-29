469cd63c180f3fe519e2cbfd581b5b6b42c986b8
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-01-29 15:05:35 +0300

    feat: add solution to "worker_threads"-task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker } from 'worker_threads';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+
+const WORKER_FILE = 'worker.js';
+const INITIAL_NUMBER = 10;
+
+const fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(fileName);
+
+const runWorker = (workerData) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(`${__dirName}/${WORKER_FILE}`, { workerData });
+    worker.on('message', resolve);
+    worker.on('error', reject);
+  });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = cpus().length;
+    const res = [];
+
+    for(let i = 0; i < cores; i++) {
+        try {
+            const data = await runWorker(INITIAL_NUMBER + i);
+            res.push({status: 'resolved', data});
+        } catch {
+            res.push({status: 'error', data: null});
+        }
+    }
+    
+    res.forEach(obj => console.log(obj));
 };
 
 await performCalculations();
